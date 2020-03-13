72c338081c757751a50d56b33519c6852808c14e
G-VINAY-KUMAR <55321137+G-VINAY-KUMAR@users.noreply.github.com>
2020-03-13 18:15:01 +0530

    Add files via upload

--- week1program3.java
+++ week1program3.java
@@ -5,20 +5,37 @@ class week1program3
     public static void main(String args[])throws IOException
     {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        //Reading Input
         int n=Integer.parseInt(br.readLine());
+        int error=0;
+        int actualop[] = new int[n+1];
+       int expectedop[] = new int[n+1];
+       //Reading Expected Output
         for(int i=0;i<=n;i++)
         {
-            int m=bitcal(i,0);
-            System.out.print(m+" ");
+            expectedop[i]=Integer.parseInt(br.readLine());     
         }
-    }
-    public static int bitcal(int n,int count)
-    {
-        if(n<1)
+        //Function calling to calculate no. of set bits
+        actualop = setbitcal(n,actualop); 
+        //checking output with expexted output
+        for(int i=0;i<=n;i++)
         {
-            return count;
+            if(actualop[i]!=expectedop[i])
+                error++;
         }
-        return bitcal(n & (n-1),count+1);
+        if(error>0)
+            System.out.println(error+" Errors");
+        else
+            System.out.println("Test case passed");
+    }
+    public static int[] setbitcal(int n,int count[])
+    {
+        count[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            count[i]=1+count[i & (i-1)];  
+        }
+        return count;
     }
 }
 
